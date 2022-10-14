1573c7cae911ab894cec768c1e285034fd7b840b
aaAbdulkadir <a.abdulkadir@hotmail.com>
2022-10-14 22:13:51 +0100

    project update

--- project/setup.md
+++ project/setup.md
@@ -5,7 +5,6 @@
 
 Something among these lines
 
-![image](images/plan.png)
 ![image](images/plan2.png)
 
 CI/CD using Jenkins on Azure Container Service (AKS)
@@ -21,6 +20,7 @@ CI/CD using Jenkins on Azure Container Service (AKS)
 - Grafana displays visualization of infrastructure and application metrics via Azure Monitor.
 - Monitor application and make improvements.
 
+
 ## Method
 
 - Create a Terraform plan to deploy:
@@ -30,10 +30,69 @@ CI/CD using Jenkins on Azure Container Service (AKS)
     - App service plan
 - After creating plan, download jenkins on the VM and connect it to the github repository with the webapp and configure webhook.
     - Build the image of the webapp, test, deploy.
-- Deploy webapp to App service so it can be used.
+- Deploy webapp to App service so it can be used. Maybe requires Jenkins connection to AKS, ACR.
+
+Jenkins:
+- Clones the GitHub repository
+- Builds a new container image
+- Pushes the container image to the ACR registry
+- Updates the image used by the AKS deployment
+
 
 
 ## References: 
 - https://learn.microsoft.com/en-us/azure/architecture/solution-ideas/articles/container-cicd-using-jenkins-and-kubernetes-on-azure-container-service
 - https://learn.microsoft.com/en-us/azure/developer/jenkins/configure-on-linux-vm
 - https://github.com/Azure-Samples/azure-voting-app-redis
+- https://learn.microsoft.com/en-us/azure/developer/jenkins/deploy-from-github-to-aks
+
+## Walkthrough
+
+### Terraform
+
+- Create a terraform file with the following:
+    - Resource group
+    - Container registry
+    - Kubernetes cluster
+    - App service plan (change size)
+    - Virtual machine
+
+    where the container registry is attached to the kubernetes cluster in the tfplan.
+
+    Once the file is ready, run the following:
+
+    ```
+    az login
+    ```
+    Log into Azure CLI so that terraform can push plan to Azure to account.
+
+    ```
+    terraform init
+
+    terraform plan -out main.tfplan
+
+    terraform apply main.tfplan
+    ```
+
+### Setting up files
+
+#### Docker
+
+- The Dockerfile exposes port 8501 which is the port streamlit listens in at. However, to make it run on Azure, the port needs to be mapped:
+    - Using a docker-compose file, make the host port 80 for 
+    Azure map onto the container port 8501 for streamlit.
+
+- Before running the docker-compose file, it needs to be given an image name that is tagged with the necessary azure extension:
+    - Add image under webapp service with the correct name
+    - Tag the image with the container registry name + .azure.io + /{image_name}:
+        - streamlit_container_registry.azure.io/portfolio-app:latest
+
+- Once the docker image is created with the correct tag, it needs to be pushed to the container registry, which will be set up on the jenkins server as on the server, it will build the image and then be pushed to the container registry.
+
+#### Kubernetes
+
+- Make the image container the same as the docker image that is going to be pushed to the container registry.
+
+### Setting up server
+
+- Log into the VM created and download 
---
+++ project/terraform/main.tf
@@ -0,0 +1,174 @@
+# create resource group
+resource "azurerm_resource_group" "resource_group" {
+  name = "${var.prefix}_project"
+  location = var.resource_group_location
+}
+
+# create storage account
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "${var.prefix}storage"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# create container registry
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.prefix}containerregistry"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+  sku                 = "Premium"
+}
+
+# create kubernetes cluster
+resource "azurerm_kubernetes_cluster" "k8_cluster" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = "${var.prefix}-aks-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2"  
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# attach ACR with AKS
+resource "azurerm_role_assignment" "enablePulling" {
+  principal_id                     = azurerm_kubernetes_cluster.k8_cluster.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+# ---------------- VM CREATION ----------------
+
+# Create virtual network
+resource "azurerm_virtual_network" "network" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "public_ip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "nsga" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.resource_group.name
+  }
+
+  byte_length = 8
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# save ssh key locally
+resource "local_file" "azure_key" {
+  filename = "keys/azure_key.pem"
+  content = tls_private_key.ssh_key.private_key_pem
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "server" {
+  name                  = "jenkins_server"
+  location              = azurerm_resource_group.resource_group.location
+  resource_group_name   = azurerm_resource_group.resource_group.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = "Standard_B4ms"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  depends_on = [
+    tls_private_key.ssh_key
+  ]
+
+  computer_name                   = "jenkins-server"
+  admin_username                  = "Abdulkadir"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "Abdulkadir"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.storage_account.primary_blob_endpoint
+  }
+}
