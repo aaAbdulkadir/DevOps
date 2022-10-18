9ade09a83600ab43382155c6031ca157ce6ccf85
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2022-10-18 02:30:27 +0100

    test

--- jenkins/bash/setup.sh
+++ jenkins/bash/setup.sh
@@ -10,8 +10,3 @@ if [ ! -f "/usr/bin/docker" ]; then
     curl https://get.docker.com | sudo bash
     sudo usermod -aG docker jenkins
 fi
-
-# install docker compose
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
