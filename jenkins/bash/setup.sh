e6c86e1e1ec284af7cb876c2336a856793a16542
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2022-10-18 02:33:29 +0100

    test

--- jenkins/bash/setup.sh
+++ jenkins/bash/setup.sh
@@ -7,6 +7,6 @@ apt-get update
 
 #installing docker
 if [ ! -f "/usr/bin/docker" ]; then
-    curl https://get.docker.com | sudo bash
-    sudo usermod -aG docker jenkins
+    curl https://get.docker.com | bash
+    usermod -aG docker jenkins
 fi
